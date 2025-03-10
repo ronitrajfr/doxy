581888a7d54fc869bf538075195a3bc313b8b4ae
ronitrajfr <ronitrajofficial7@gmail.com>
2025-03-10 14:41:14 +0530

    some tool for the editor

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -8,9 +8,39 @@ import Table from "@tiptap/extension-table";
 import TableCell from "@tiptap/extension-table-cell";
 import TableHeader from "@tiptap/extension-table-header";
 import TableRow from "@tiptap/extension-table-row";
+import Underline from "@tiptap/extension-underline";
+import Image from "@tiptap/extension-image";
+import ImageResize from "tiptap-extension-resize-image";
+import { useEditorStore } from "@/store/use-editor-store";
 
 const Editor = () => {
+  const { setEditor } = useEditorStore();
+
   const editor = useEditor({
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
+    onDestroy() {
+      setEditor(null);
+    },
+    onUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onSelectionUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onTransaction({ editor }) {
+      setEditor(editor);
+    },
+    onFocus({ editor }) {
+      setEditor(editor);
+    },
+    onBlur({ editor }) {
+      setEditor(editor);
+    },
+    onContentError({ editor }) {
+      setEditor(editor);
+    },
     editorProps: {
       attributes: {
         style: "padding-left: 56px; padding-right: 56px;",
@@ -28,6 +58,9 @@ const Editor = () => {
       TableRow,
       TableHeader,
       TableCell,
+      Image,
+      ImageResize,
+      Underline,
     ],
     content: `
         <table>
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Editor from "./editor";
+import Toolbar from "./toolbar";
 
 interface DocumentIdPageProps {
   params: Promise<{ documentId: string }>;
@@ -9,6 +10,7 @@ const page = async ({ params }: DocumentIdPageProps) => {
   const { documentId } = await params;
   return (
     <div className="min-h-screen bg-[#FAFBFD]">
+      <Toolbar />
       <Editor />
     </div>
   );
